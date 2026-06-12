5c19d0394fca1b876c625e9f21b11a36b86f912f
MorseWayne <morsewayne98@gmail.com>
2026-06-12 10:52:59 +0800

    fix: correct server configuration in vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -31,13 +31,14 @@ export default defineConfig({
     },
   },
   server: {
+    host: '0.0.0.0',
     proxy: {
       '/api': {
         target: 'http://127.0.0.1:8000',
         changeOrigin: true,
       },
     },
-  },
+  }
 
   // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
